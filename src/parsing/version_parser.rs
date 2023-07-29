3e4887b602f1146d93ee2754623236e8d161698f
notPlancha <andre.plancha@hotmail.com>
2023-07-29 02:53:13 +0100

    parsers probably done, needs building to go further

--- src/parsing/version_parser.rs
+++ src/parsing/version_parser.rs
@@ -1,6 +1,7 @@
 #[macro_use] extern crate lalrpop_util;
 
 use std::cmp::Ordering;
+use std::fmt::Display;
 use std::str::FromStr;
 use lalrpop_util::lalrpop_mod;
 use thiserror::Error;
@@ -37,10 +38,11 @@ impl Version {
     //1.1.0.1.5 < 1.1.0.1.6, 1.1.0.1.5 > 1.1.0, 1.1.0.0.0 > 1.1.0
     extra_version: Option<String>,
     // 1.1.0-rc.1 < 1.1.0-rc.2, 1-a < 1-b, 1.1.0-rc.1 <= 1.1.0
-    // é menor que ele mas no range é igual, tipo uma espécie de epsilon
-    // isto é porque o range espera-se que por exemplo >= 1.0, < 2.0 não inclua 2.0-alpha
-    // embora tecnicamente inclui pq é antes
-    // ainda assim quando for para comparar versões, 2.0-alpha é menor que 2.0 na mesma (por exemplo pra atualizar)
+    /// # Pre-release-note
+    /// é menor que ele mas no range é igual, tipo uma espécie de epsilon
+    /// isto é porque o range espera-se que por exemplo >= 1.0, < 2.0 não inclua 2.0-alpha
+    /// embora tecnicamente inclui pq é antes
+    /// ainda assim quando for para comparar versões, 2.0-alpha é menor que 2.0 na mesma (por exemplo pra atualizar)
     pre_release: Option<String>,
     //1.1.0+build.1 = 1.1.0+build.2, 1.1.0+build.1 = 1.1.0
     build: Option<String>
@@ -116,6 +118,25 @@ impl Version {
       build,
     }
   }
+
+  fn is(&self, other: &Self) -> bool {
+    // comparasion with everything, and not equivelant
+    self.major == other.major
+      && self.minor == other.minor
+      && self.patch == other.patch
+      && self.extra_version == other.extra_version
+      && self.pre_release == other.pre_release
+      && self.build == other.build
+  }
+  fn is_older_than(&self, other: &Self) -> bool {
+    // comparasion with everything, different than <= since pre_release is checked
+    // check version_parser.rs#Pre-release-note
+    self.major < other.major
+      || self.minor < other.minor
+      || self.patch < other.patch
+      || self.extra_version < other.extra_version
+      || self.pre_release < other.pre_release
+  }
 }
 
 impl FromStr for Version {
@@ -126,70 +147,140 @@ impl FromStr for Version {
   }
 }
 
-impl Eq for Version {
-
-}
-
 impl PartialEq for Version {
   fn eq(&self, other: &Self) -> bool {
-    todo!()
+    self.major == other.major
+      && self.minor == other.minor
+      && self.patch == other.patch
+      && self.extra_version == other.extra_version
+      && self.pre_release == other.pre_release
   }
 }
+
+impl Eq for Version {}
 
 impl PartialOrd<Version> for Version {
   fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-    todo!()
+    if self.major < other.major {
+      Some(Ordering::Less)
+    } else if self.major > other.major {
+      Some(Ordering::Greater)
+    } else if self.minor < other.minor {
+      Some(Ordering::Less)
+    } else if self.minor > other.minor {
+      Some(Ordering::Greater)
+    } else if self.patch < other.patch {
+      Some(Ordering::Less)
+    } else if self.patch > other.patch {
+      Some(Ordering::Greater)
+    } else if self.extra_version < other.extra_version { //TODO check if this works since it's optional
+      Some(Ordering::Less)
+    } else if self.extra_version > other.extra_version {
+      Some(Ordering::Greater)
+      // pre-release isn't checked because this is for implmenting ranges, see version_parser.rs#Pre-release-note
+    } else {
+      Some(Ordering::Equal)
+    }
+  }
+}
+impl Ord for Version {
+  fn cmp(&self, other: &Self) -> Ordering {
+    self.partial_cmp(other).unwrap()
   }
 }
 
-impl Ord for Version {
-  fn cmp(&self, other: &Self) -> Ordering {
-    todo!()
+impl ToString for Version {
+  fn to_string(&self) -> String {
+    let mut s = format!("{}.{}.{}", self.major, self.minor, self.patch);
+    if let Some(extra_version) = &self.extra_version {
+      s.push_str(&format!(".{}", extra_version));
+    }
+    if let Some(pre_release) = &self.pre_release {
+      s.push_str(&format!("-{}", pre_release));
+    }
+    if let Some(build) = &self.build {
+      s.push_str(&format!("+{}", build));
+    }
+    s
   }
 }
+
 
 struct Range {
   min: Option<Version>, //inclusive
   max: Option<Version>, //exclusive, because it's hard to go back to the previous version
-  except_ranges: Vec<Range>,
-  except_versions: Vec<Version>,
+  except: Vec<Version>,
+  include: Vec<Version>,
+}
+
+impl ToString for Range {
+  fn to_string(&self) -> String {
+    if self.is_any() {
+      return "*".to_string();
+    }
+    let mut s = String::new();
+    if let Some(min) = &self.min {
+      s.push_str(&format!(">={},", min.to_string()));
+    }
+    if let Some(max) = &self.max {
+      s.push_str(&format!("<{},", max.to_string()));
+    }
+    for except in &self.except {
+      s.push_str(&format!("!={},", except.to_string()));
+    }
+    for include in &self.include {
+      s.push_str(&format!("={},", include.to_string()));
+    }
+    s.pop(); //remove the last comma
+    s
+  }
 }
 
 impl Range {
   fn contains(&self, version: Version) -> bool {
     todo!()
   }
-  fn is_any(&self) -> bool { // == is empty
+  fn is_any(&self) -> bool { // is empty or is just >= 0.0.0
     todo!()
   }
-  fn is_valid(&self) -> bool {
+  fn is_valid(&self) -> bool { // is not empty and min <= max and is not < 0.0.0
     todo!()
   }
-  fn is_exact_match(&self) -> bool {
+  fn is_exact_match(&self) -> bool { // min == max or just includes one version
     todo!()
-  }
-  fn new(
-    min: Option<Version>,
-    max: Option<Version>,
-    except_ranges: Vec<Range>,
-    except_versions: Vec<Version>,
-  ) -> Self {
-    Self {
-      min,
-      max,
-      except_ranges,
-      except_versions,
-    }
   }
   fn from_ver_vec(ranges: Vec<(Op, Version)>) -> Self {
-
+    // Sort the ranges by version number
+    let mut ranges = Self::sort_vec(ranges);
+    // get lowest ge, and highest lt (since it's sorted)
+    let min = ranges.first().map(|(_, v)| *v);
+    let max = ranges.last().map(|(_, v)| *v);
+    // get all the except and include (Ne and Eq)
+    let except: Vec<_> = ranges
+      .into_iter()
+      .filter(|(op, _)| *op == Op::Ne)
+      .map(|(_, v)| v)
+      .collect();
+    let include: Vec<_> = ranges
+      .into_iter()
+      .filter(|(op, _)| *op == Op::Eq)
+      .map(|(_, v)| v)
+      .collect();
+    Range { //Note: this can return an invalid range, that's why we have is_valid
+      min,
+      max,
+      except,
+      include,
+    }
   }
   fn mixed_vec_to_stand_vec(ranges: Vec<(Op, Version)>) -> Vec<(Op, Version)> {
-    // Expand tilde and caret ranges to simple Gt/Lt/Ge/Le ranges
+    // Expand tilde, caret, le and gt ranges to simple lt and ge ranges
     ranges.into_iter().flat_map(|(op, version)| {
       match op {
         Op::Tilde => Self::tilde_range_to_vec(version),
         Op::Caret => Self::caret_range_to_vec(version),
+        Op::Le => Self::le_range_to_vec(version),
+        Op::Gt => Self::gt_range_to_vec(version),
         _ => vec![(op, version)],
       }
     }).collect::<Vec<_>>()
@@ -197,7 +288,8 @@ impl Range {
 
   fn sort_vec(ranges: Vec<(Op, Version)>) -> Vec<(Op, Version)> {
     // Sort the ranges by version number
-    let mut ranges = ranges;
+
+    let mut ranges = Self.mixed_vec_to_stand_vec(ranges);
     ranges.sort_by(|(_, a), (_, b)| a.cmp(&b));
     ranges
   }
@@ -226,6 +318,23 @@ impl Range {
       (Op::Lt, Version::new(version.major + 1, 0, 0, None, None, None)),
     ]
   }
+  fn le_range_to_lt(version: Version) -> Vec<(Op, Version)> {
+    // <=1.2.3 -> <1.2.4
+    // <=1.2 -> <1.2.1
+    // <=1 -> <1.0.1
+    vec![
+      (Op::Lt, Version::new(version.major, version.minor, version.patch + 1, None, None, None)),
+    ]
+  }
+  fn gt_range_to_ge(version: Version) -> Vec<(Op, Version)> {
+    // >1.2.3 -> >=1.2.4
+    // >1.2 -> >=1.2.1
+    // >1 -> >=1.0.1
+    vec![
+      (Op::Ge, Version::new(version.major, version.minor, version.patch + 1, None, None, None)),
+    ]
+  }
+
 }
 
 pub enum Op {
